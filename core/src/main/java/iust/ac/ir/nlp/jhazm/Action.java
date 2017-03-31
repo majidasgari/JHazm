84cc0cf4da228aed00013b68893317ec69fe40ca
maJid~ASGARI <majid.asgari@gmail.com>
2017-04-01 01:23:50 +0430

    moving package to better place rename methods to obey java conventions for method names

--- core/src/main/java/iust/ac/ir/nlp/jhazm/Action.java
+++
@@ -1,9 +0,0 @@
-package iust.ac.ir.nlp.jhazm;
-
-/**
- * Created by Majid Asgari
- */
-public enum Action {
-    Normalizing, SentenceTokenizing, WorkTokenizing, Stemming,
-    Lemmatize, DependencyParsing, PartOfSpeechTagging
-}
